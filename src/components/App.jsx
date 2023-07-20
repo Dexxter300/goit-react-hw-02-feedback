ef6a0ac0b89977698b40a3e3030cc6849f8458be
Dexxter300 <daniilsafarov2005@gmail.com>
2023-07-21 02:21:32 +0300

    4/4 tasks done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import { Statistics } from './statistics/statistics';
+import { FeedbackOptions } from './feedbackOptions/feedbackOptions';
+import { Section } from './section/section';
+import { Notification } from './notification/notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    netural: 0,
+    bad: 0,
+  };
+  total = 0;
+  positiveFeedbackPercentage = 0;
+
+  changeStats = option => {
+    // console.log(event.target.textContent);
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+    this.countTotalFeedback();
+    this.countPositiveFeedbackPercentage();
+  };
+
+  countTotalFeedback = () => {
+    this.total = this.state.good + this.state.netural + this.state.bad + 1;
+    // console.log(this.total);
+    // return this.total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    this.positiveFeedbackPercentage = (this.state.good / this.total) * 100;
+    // console.log(this.positiveFeedbackPercentage);
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          paddingLeft: '100px',
+        }}
+      >
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.changeStats}
+          ></FeedbackOptions>
+        </Section>
+        <Section title={'Statistic'}>
+          {this.total > 0 ? (
+            <Statistics
+              good={this.state.good}
+              netural={this.state.netural}
+              bad={this.state.bad}
+              total={this.total}
+              positive={this.positiveFeedbackPercentage}
+            ></Statistics>
+          ) : (
+            <Notification message={'There is no feedback'}></Notification>
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
